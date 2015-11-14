f662113114d8fcfd9a7dcc11d50269d70da54d6d
LorenHan <lorenhan@163.com>
2015-11-14 18:55:56 +0800

    模拟器上可以显示图片，手机上无法显示，需要将图片资源加载到程序中。

--- 02-按钮操作/02-按钮操作/ViewController.m
+++ 02-按钮操作/02-按钮操作/ViewController.m
@@ -8,7 +8,20 @@
 
 #import "ViewController.h"
 
+enum{
+    keyMovingTop,
+    keyMovingBottom,
+    keyMovingLeft,
+    keyMovingRight,
+    keyZoomPlus,
+    keyZoomMinus,
+    keyRotateLeft,
+    keyRotateRight,
+};
+
 @interface ViewController ()
+
+@property (nonatomic,weak) IBOutlet UIButton *iconButton;
 
 @end
 
@@ -17,6 +30,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //设置图片按钮位置
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    
+    //设置图片背景
+    [btn setBackgroundImage:[UIImage imageNamed:@"btn01.png"] forState:UIControlStateNormal];
+    [btn setBackgroundImage:[UIImage imageNamed:@"btn02.png"] forState:UIControlStateHighlighted];
+    
+    //设置背景颜色
+    [btn setBackgroundColor:[UIColor greenColor]];
+    
+    //设置文字
+    [btn setTitle:@"点我啊" forState:UIControlStateNormal];
+    [btn setTitle:@"被摸了" forState:UIControlStateHighlighted];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    
+    btn.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    
+    [self.view addSubview:btn];
+    
+    self.iconButton = btn;
+}
+
+- (IBAction)iconMove:(UIButton *)button{
+    CGFloat tx = 0,ty = 0;
+    switch (button.tag) {
+        case keyMovingTop:
+            ty -= 20;
+            break;
+        case keyMovingBottom:
+            ty += 20;
+            break;
+        case keyMovingLeft:
+            tx -= 20;
+            break;
+        case keyMovingRight:
+            tx += 20;
+            break;
+        default:
+            break;
+    }
+    
+    //设置位置  动画效果
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationBeginsFromCurrentState:2.0f];
+    self.iconButton.transform = CGAffineTransformTranslate(self.iconButton.transform, tx, ty);
+    [UIView commitAnimations];
+}
+
+- (IBAction)zoom:(UIButton *)button{
+    CGFloat scale = (button.tag == keyZoomPlus) ? 1.2 : 0.8;
+    
+    [UIView beginAnimations:nil context:nil];
+    self.iconButton.transform = CGAffineTransformScale(self.iconButton.transform, scale, scale);
+    [UIView commitAnimations];
+}
+
+- (IBAction)rotate:(UIButton *)button{
+    CGFloat angle = (button.tag == keyRotateLeft) ? -M_PI_4 :M_PI_4;
+    
+    [UIView beginAnimations:nil context:nil];
+    self.iconButton.transform = CGAffineTransformRotate(self.iconButton.transform, angle) ;
 }
 
 - (void)didReceiveMemoryWarning {
